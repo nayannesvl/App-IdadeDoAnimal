e9764e1ccd71dc4a232b9cfef8c1bdb9dc1cf055
Nayanne <nayannesantanav@gmail.com>
2022-06-22 11:41:42 -0300

    Initial Commit

---
+++ IdadeDoAnimal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  Created by Nayanne Sant'Ana on 06/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "A idade do cachorro é: " + String(idade)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
